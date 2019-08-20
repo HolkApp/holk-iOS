4d51fa6e171b8093bdd3000ea8c7624a60d4e212
张梦皓 <zmhand1@gmail.com>
2019-08-20 21:01:37 +0200

    Added the custom button

---
+++ Holk/Views/HolkButton.swift
@@ -0,0 +1,24 @@
+//
+//  HolkButton.swift
+//  Holk
+//
+//  Created by 张梦皓 on 2019-08-20.
+//  Copyright © 2019 Holk. All rights reserved.
+//
+
+import UIKit
+
+class HolkButton: UIButton {
+    override var isEnabled: Bool {
+        didSet {
+            guard let backgroundColor = backgroundColor else { return }
+            if isEnabled {
+                let alpha = min(backgroundColor.cgColor.alpha / 0.4, 1)
+                self.backgroundColor = backgroundColor.withAlphaComponent(alpha)
+            } else {
+                self.backgroundColor = backgroundColor.withAlphaComponent(backgroundColor.cgColor.alpha * 0.4)
+            }
+        }
+    }
+}
+
