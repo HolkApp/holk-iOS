5aa74aee164ddbf1519a28d52478f282c6e15611
张梦皓 <zmhand1@gmail.com>
2019-09-10 23:09:13 +0200

    Updated the decoding key

--- Holk/Onboarding/Model/LoginToken.swift
+++ Holk/Onboarding/Model/LoginToken.swift
@@ -9,10 +9,32 @@
 import Foundation
 
 struct LoginToken: Codable {
-    var access_token: String
-    var refresh_token: String
-    var token_type: String
+    var accessToken: String
+    var refreshToken: String
+    var tokenType: String
     var scope: String
     var jti: String
-    var expires_in: Int
+    var expiresInSeconds: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case refreshToken = "refresh_token"
+        case tokenType = "token_type"
+        case scope = "scope"
+        case jti = "jti"
+        case expiresInSeconds = "expires_in"
+    }
+}
+
+extension LoginToken {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        accessToken = try container.decode(String.self, forKey: .accessToken)
+        refreshToken = try container.decode(String.self, forKey: .refreshToken)
+        tokenType = try container.decode(String.self, forKey: .tokenType)
+        scope = try container.decode(String.self, forKey: .scope)
+        jti = try container.decode(String.self, forKey: .jti)
+        expiresInSeconds = try container.decode(Int.self, forKey: .expiresInSeconds)
+    }
 }
