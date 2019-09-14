a4c522d4c7ed0c9a880ba40eb14e72fb56e5d3ab
张梦皓 <zmhand1@gmail.com>
2019-09-14 17:19:59 +0200

    Edited the color to use the asset

--- Holk/Color.swift
+++ Holk/Color.swift
@@ -10,38 +10,38 @@ import UIKit
 
 enum Color {
     static var mainBackgroundColor: UIColor {
-        return UIColor(red: 254/255, green: 206/255, blue: 0/255, alpha: 1)
+        return UIColor(named: #function)!
     }
     
     static var secondaryBackgroundColor: UIColor {
-        return UIColor.white
+        return UIColor(named: #function)!
     }
     
     static var placeHolderTextColor: UIColor {
-        return UIColor(red: 151/255, green: 151/255, blue: 151/255, alpha: 1)
+        return UIColor(named: #function)!
     }
     
     static var mainForegroundColor: UIColor {
-        return UIColor(red: 15/255, green: 15/255, blue: 15/255, alpha: 1)
+        return UIColor(named: #function)!
     }
     
     static var secondaryForegroundColor: UIColor {
-        return UIColor(red: 66/255, green: 65/255, blue: 65/255, alpha: 1)
+        return UIColor(named: #function)!
     }
     
     static var onBoardingBackgroundColor: UIColor {
-        return UIColor(red: 222/255, green: 222/255, blue: 222/255, alpha: 1)
+        return UIColor(named: #function)!
     }
     
     static var tabbarBackgroundColor: UIColor {
-        return UIColor.white
+        return UIColor(named: #function)!
     }
     
     static var lightBorderColor: UIColor {
-        return UIColor(red: 241/255, green: 226/255, blue: 226/255, alpha: 1)
+        return UIColor(named: #function)!
     }
     
     static var mainButtonBackgroundColor: UIColor {
-        return UIColor(red: 242/255, green: 178/255, blue: 164/255, alpha: 1)
+        return UIColor(named: #function)!
     }
 }
