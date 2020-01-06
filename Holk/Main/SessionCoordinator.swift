9a4b73e05f0bedcfb9077e080900951274e2ad76
张梦皓 <zmhand1@gmail.com>
2020-01-06 15:06:37 +0100

    Set the session coordinator to handle the navigation

--- Holk/Main/SessionCoordinator.swift
+++ Holk/Main/SessionCoordinator.swift
@@ -7,28 +7,51 @@
 //
 
 import UIKit
+import RxSwift
 
 // TODO: Use SessionCoordinator instead of OnboardingCoordinator for the login, signup check
 final class SessionCoordinator: NSObject, Coordinator, UINavigationControllerDelegate {
     // MARK: - Public Properties
     var navController: UINavigationController
     var storeController: StoreController
+    
     private var onboardingCoordinator: OnboardingCoordinator
+    private let bag = DisposeBag()
+    
     // MARK: - Init
     init(navController: UINavigationController) {
         self.navController = navController
         self.storeController = StoreController()
         onboardingCoordinator = OnboardingCoordinator(navController: navController, storeController: storeController)
+        super.init()
+        
+        storeController.delegate = self
     }
     
     func start() {
-        let landingViewController = StoryboardScene.Onboarding.landingViewController.instantiate()
-        landingViewController.coordinator = self
         navController.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         navController.navigationBar.shadowImage = UIImage()
         navController.delegate = self
         navController.navigationBar.tintColor = .black
-        navController.pushViewController(landingViewController, animated: true)
+        switch storeController.sessionState {
+        case .newSession:
+            let landingViewController = StoryboardScene.Onboarding.landingViewController.instantiate()
+            landingViewController.coordinator = self
+            if navController.presentedViewController != nil {
+                navController.dismiss(animated: true) {
+                    if !(self.navController.topViewController is LandingViewController) {
+                        self.navController.pushViewController(landingViewController, animated: true)
+                    }
+                }
+            } else {
+                navController.pushViewController(landingViewController, animated: false)
+            }
+        case .shouldRefresh:
+            showLoading()
+            storeController.authenticationStore.refresh().subscribe().disposed(by: bag)
+        case .updated:
+            showSession()
+        }
     }
     
     func showLogin(presentByRoot: Bool = false) {
@@ -67,7 +90,9 @@ final class SessionCoordinator: NSObject, Coordinator, UINavigationControllerDel
         let tabbarController = TabBarController()
         tabbarController.coordinator = self
         tabbarController.modalPresentationStyle = .overFullScreen
-        if navController.presentedViewController != nil {
+        if navController.viewControllers.isEmpty {
+            navController.pushViewController(tabbarController, animated: true)
+        } else if navController.presentedViewController != nil {
                 navController.dismiss(animated: true) {
                     self.navController.present(tabbarController, animated: true) {
                         self.navController.popToRootViewController(animated: false)
@@ -83,6 +108,17 @@ final class SessionCoordinator: NSObject, Coordinator, UINavigationControllerDel
     
     func logout() {
         storeController.resetSession()
+        start()
+    }
+}
+
+extension SessionCoordinator: StoreControllerDelegate {
+    func storeControllerAccessTokenUpdated() {
+        showSession()
+    }
+    
+    func storeControllerRefreshTokenExpired() {
+        logout()
     }
 }
 
