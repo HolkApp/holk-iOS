3b9fc5218c0b42617450941e9ec739aeb01c9b64
张梦皓 <zmhand1@gmail.com>
2020-01-12 23:06:15 +0100

    Fixed the custom page control

--- Holk/Onboarding/OnboardingInfoPageViewController.swift
+++ Holk/Onboarding/OnboardingInfoPageViewController.swift
@@ -1,11 +1,16 @@
 import UIKit
 
 final class OnboardingInfoPageViewController: UIPageViewController {
+    // MARK: - Private Variables
+    private var pageControl = UIPageControl()
+    private var pendingIndex: Int?
+    
     // MARK: - Public Variables
     var orderedViewControllers = [UIViewController]()
     
     override func viewDidLoad() {
         dataSource = self
+        delegate = self
         
         let infoViewController = StoryboardScene.Onboarding.onboardingInfoViewController.instantiate()
         let secondViewController = UIViewController()
@@ -13,8 +18,19 @@ final class OnboardingInfoPageViewController: UIPageViewController {
         let thirdViewController = UIViewController()
         thirdViewController.view.backgroundColor = .systemGreen
         orderedViewControllers = [infoViewController, secondViewController, thirdViewController]
-        
         setViewControllers([infoViewController], direction: .forward, animated: true, completion: nil)
+        
+        pageControl.numberOfPages = orderedViewControllers.count
+        pageControl.frame = CGRect(origin: .zero, size: pageControl.size(forNumberOfPages: orderedViewControllers.count))
+        pageControl.currentPageIndicatorTintColor = Color.mainHighlightColor
+        pageControl.pageIndicatorTintColor = Color.mainForegroundColor
+        view.addSubview(pageControl)
+        
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -166)
+        ])
     }
 }
 
@@ -55,15 +71,41 @@ extension OnboardingInfoPageViewController: UIPageViewControllerDataSource {
         
         return orderedViewControllers[nextIndex]
     }
-    
-    func presentationCount(for pageViewController: UIPageViewController) -> Int {
-        orderedViewControllers.count
+}
+
+extension OnboardingInfoPageViewController: UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = orderedViewControllers.firstIndex(of: viewController) else {
+            return nil
+        }
+        if currentIndex == 0 {
+            return nil
+        }
+        let previousIndex = abs((currentIndex - 1) % orderedViewControllers.count)
+        return orderedViewControllers[previousIndex]
     }
-    
-    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        let firstViewControllerIndex = viewControllers?.first.flatMap(orderedViewControllers.firstIndex(of: ))
-        
-        return firstViewControllerIndex ?? 0
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = orderedViewControllers.firstIndex(of: viewController) else {
+            return nil
+        }
+        if currentIndex == orderedViewControllers.count-1 {
+            return nil
+        }
+        let nextIndex = abs((currentIndex + 1) % orderedViewControllers.count)
+        return orderedViewControllers[nextIndex]
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        pendingIndex = orderedViewControllers.firstIndex(of: pendingViewControllers.first!)
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed {
+            if let index = pendingIndex {
+                pageControl.currentPage = index
+            }
+        }
     }
     
 }
