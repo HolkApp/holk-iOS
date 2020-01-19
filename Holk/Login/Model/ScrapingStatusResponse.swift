580c06733b232bdf32d8f564513af2d0db95f80d
张梦皓 <zmhand1@gmail.com>
2020-01-19 15:07:58 +0100

    Re-orgnized the files

---
+++ Holk/Login/Model/ScrapingStatusResponse.swift
@@ -0,0 +1,14 @@
+//
+//  ScrapingStatusResponse.swift
+//  Holk
+//
+//  Created by 张梦皓 on 2020-01-14.
+//  Copyright © 2020 Holk. All rights reserved.
+//
+
+import Foundation
+
+struct ScrapingStatusResponse: Codable {
+    var scrapingStatus: String
+}
+
---
+++ Holk/Stores/Task/PollingTask.swift
@@ -0,0 +1,9 @@
+//
+//  PollingTask.swift
+//  Holk
+//
+//  Created by 张梦皓 on 2020-01-14.
+//  Copyright © 2020 Holk. All rights reserved.
+//
+
+import Foundation
