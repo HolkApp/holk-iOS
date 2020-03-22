8d45e61acbe142d3e8ace38083ae7549cec435f9
张梦皓 <zmhand1@gmail.com>
2020-03-22 22:31:09 +0100

    Polling and handle the background task disconnected error with retry

--- Holk/Stores/Task/PollingTask.swift
+++ Holk/Stores/Task/PollingTask.swift
@@ -9,14 +9,36 @@
 import Foundation
 import RxSwift
 
+private var retryInterval: Int = 1
+
 final class ScrapingStatusPollingTask {
-    private var retryInterval: Int = 2
+
     func poll<T: Codable, E: Error>(_ observable: Observable<Result<T, E>>, pollingUntil: @escaping (Result<T, E>) -> Bool) -> Observable<Result<T, E>> {
-        let interval = Observable<Int>.interval(.seconds(retryInterval), scheduler: MainScheduler.instance)
+        let timer = Observable<Int>.timer(.seconds(retryInterval), scheduler: MainScheduler.instance)
         // 3. Interval subscription
-        let subscription = interval.flatMapLatest { index in
+        let subscription = timer.flatMapLatest { index in
             observable
         }.takeUntil(.inclusive, predicate: pollingUntil)
         return subscription
+    }
+}
+
+extension ObservableType {
+    func badNetworkRetrier(_ maxAttemptCount: Int = Int.max,
+                 shouldRetry: @escaping (Error) -> Bool = { ($0 as NSError).code == -1005 }) -> Observable<Element> {
+        return retryWhen { (errors: Observable<Error>) in
+            return errors.enumerated().flatMap { attempt, error -> Observable<Void> in
+                guard shouldRetry(error), maxAttemptCount > attempt + 1 else {
+                    return .error(error)
+                }
+
+                let timer = Observable<Int>.timer(
+                    RxTimeInterval.seconds(retryInterval),
+                    scheduler: MainScheduler.instance
+                ).map { _ in () } // cast to Observable<Void>
+
+                return timer
+            }
+        }
     }
 }
