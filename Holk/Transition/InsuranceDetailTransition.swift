f5cd9d28a07fc1b2a55f745b8bb6907992d69ad0
张梦皓 <zmhand1@gmail.com>
2020-04-11 22:28:48 +0200

    Transition animation updates

--- Holk/Transition/InsuranceDetailTransition.swift
+++ Holk/Transition/InsuranceDetailTransition.swift
@@ -26,42 +26,48 @@ final class InsuranceDetailTransition: NSObject, UIViewControllerAnimatedTransit
         if let selectedIndexPath = fromViewController.collectionView.indexPathsForSelectedItems?.first, let fromCell = fromViewController.collectionView.cellForItem(at: selectedIndexPath) as? InsuranceCollectionViewCell, let toView = toViewController.view {
 
             let containerView = transitionContext.containerView
-            containerView.backgroundColor = .white
+            containerView.backgroundColor = .clear
             containerView.addSubview(toViewController.view)
             toViewController.view.alpha = 0
             toViewController.view.layoutIfNeeded()
 
             let temporaryView = UIView()
-            temporaryView.frame = fromCell.frame.offsetBy(dx: 0, dy: 140)
+
+            temporaryView.frame = fromCell.convert(fromCell.bounds, to: containerView)
+            var a = UIView()
+            if let ringChart = fromCell.ringChart.snapshotView(afterScreenUpdates: false) {
+                a = ringChart
+                ringChart.frame = fromCell.ringChart.convert(ringChart.frame, to: temporaryView)
+                temporaryView.addSubview(ringChart)
+            }
             temporaryView.backgroundColor = .white
             containerView.addSubview(temporaryView)
 
             let duration = transitionDuration(using: transitionContext)
 
-            UIView.animate(withDuration: duration / 2,
-                           delay: 0,
-                           options: UIView.AnimationOptions.curveEaseInOut,
-                           animations: {
-                            fromViewController.view.alpha = 0
-            })
+            UIView.animate(
+                withDuration: duration / 2,
+                delay: 0,
+                options: UIView.AnimationOptions.curveEaseInOut,
+                animations: {
+                    fromViewController.view.alpha = 0
+                    temporaryView.frame = toView.frame
+                    a.frame = toViewController.ringChart.frame
+                    toViewController.view.alpha = 1.0
+                },
+                completion: { success in
+                    transitionContext.completeTransition(success)
+                    fromViewController.view.alpha = 1
+                    fromCell.alpha = 1.0
+                    temporaryView.removeFromSuperview()
+                })
 
-            UIView.animate(withDuration: duration / 2,
-                           delay: 0,
-                           options: UIView.AnimationOptions.curveEaseInOut,
-                           animations: {
-                            temporaryView.frame = toView.frame
-            })
-
-            UIView.animate(withDuration: duration / 2,
-                           delay: duration / 2,
-                           options: UIView.AnimationOptions.curveEaseInOut,
-                           animations: {
-                            toViewController.view.alpha = 1.0
-            }, completion: { success in
-                transitionContext.completeTransition(success)
-                fromViewController.view.alpha = 1
-                temporaryView.removeFromSuperview()
-                fromCell.alpha = 1.0
+            UIView.animate(
+            withDuration: duration / 4,
+            delay: duration / 2,
+            options: UIView.AnimationOptions.curveEaseInOut,
+            animations: {
+                temporaryView.alpha = 0
             })
         }
     }
