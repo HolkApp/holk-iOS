b48e5f00cfb17589e09c4b6d0ab4e6ac9162c0d0
张梦皓 <zmhand1@gmail.com>
2020-05-17 23:32:24 +0200

    Add new cell for onboarding orthogonal

--- Holk/Custom/CustomView/CustomCollectionLayout/UICollectionViewCompositionalLayout+extension.swift
+++ Holk/Custom/CustomView/CustomCollectionLayout/UICollectionViewCompositionalLayout+extension.swift
@@ -18,6 +18,16 @@ extension UICollectionViewCompositionalLayout {
         return layout
     }
 
+    static func makeOnboardingViewSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let hintSection = NSCollectionLayoutSection(group: group)
+        hintSection.orthogonalScrollingBehavior = .groupPaging
+        return hintSection
+    }
+
     private static func makeHintSection() -> NSCollectionLayoutSection {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
