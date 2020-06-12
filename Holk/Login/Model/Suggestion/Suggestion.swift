7d4eded8d26f42b4fae55b205c60703529a926ca
Menghao Zhang <zmhand1@gmail.com>
2020-06-12 19:46:29 +0200

    Added the initial suggestion model

--- Holk/Login/Model/Suggestion/Suggestion.swift
+++ Holk/Login/Model/Suggestion/Suggestion.swift
@@ -1,9 +1,7 @@
-//
-//  Suggestion.swift
-//  Holk
-//
-//  Created by 张梦皓 on 2020-06-11.
-//  Copyright © 2020 Holk. All rights reserved.
-//
-
 import Foundation
+
+struct Suggestion: Codable {
+    let header: String
+    let body: String
+    let icon: URL
+}
--- Holk/Login/Model/Suggestion/SuggestionsListResponse.swift
+++ Holk/Login/Model/Suggestion/SuggestionsListResponse.swift
@@ -1,9 +1,6 @@
-//
-//  SuggestionListResponse.swift
-//  Holk
-//
-//  Created by 张梦皓 on 2020-06-11.
-//  Copyright © 2020 Holk. All rights reserved.
-//
-
 import Foundation
+
+struct SuggestionsListResponse: Codable {
+    let gaps: [Suggestion]
+    let thinkOfs: [Suggestion]
+}
