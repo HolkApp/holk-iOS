2198964c44e5c806a67edf5cbda51e30f3906387
Menghao Zhang <zmhand1@gmail.com>
2020-06-21 22:04:00 +0200

    added image for home insurance cell

--- Holk/ListInsurance/InsuranceOverview/Cells/HomeInsuranceCell/HomeInsurnaceCollectionViewCell.swift
+++ Holk/ListInsurance/InsuranceOverview/Cells/HomeInsuranceCell/HomeInsurnaceCollectionViewCell.swift
@@ -143,6 +143,8 @@ final class HomeinsuranceCollectionViewCell: UICollectionViewCell {
             insuranceImageView.topAnchor.constraint(equalTo: ringChart.bottomAnchor, constant: 36),
             insuranceImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
             insuranceImageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -16),
+            insuranceImageView.widthAnchor.constraint(equalToConstant: 120),
+            insuranceImageView.heightAnchor.constraint(equalToConstant: 48),
 
             chevronView.widthAnchor.constraint(equalToConstant: 16),
             chevronView.heightAnchor.constraint(equalToConstant: 24),
