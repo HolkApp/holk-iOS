a71bde896342aebaeaa8594743d77031d17630ec
Menghao Zhang <zmhand1@gmail.com>
2020-07-19 20:44:35 +0200

    banner view and header

---
+++ Holk/ListInsurance/InsurnaceSuggestion/View/ThinkOfSuggestionDetail/ThinkOfBannerCollectionViewCell.swift
@@ -0,0 +1,79 @@
+//
+//  ThinkOfBannerCollectionViewCell.swift
+//  Holk
+//
+//  Created by 张梦皓 on 2020-07-19.
+//  Copyright © 2020 Holk. All rights reserved.
+//
+
+import UIKit
+
+final class ThinkOfBannerCollectionViewCell: UICollectionViewCell {
+    private let imageView = UIImageView()
+    private let headerLabel = UILabel()
+    private let descriptionLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+
+    func configure(_ viewModel: ThinkOfSuggestionBannerViewModel) {
+        if let imageURL = viewModel.imageURL {
+            UIImage.makeImage(imageURL) { [weak self] image in
+                self?.imageView.image = image
+            }
+        } else {
+            imageView.image = UIImage(named: "thinkOfPlaceholder")
+        }
+
+        headerLabel.text = viewModel.detailHeader
+        descriptionLabel.text = viewModel.detailDescription
+    }
+
+    private func setup() {
+//        clipsToBounds = true
+        backgroundColor = Color.secondaryBackground
+
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        headerLabel.setStyleGuide(.header5)
+        headerLabel.numberOfLines = 0
+        headerLabel.textColor = Color.mainForeground
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        descriptionLabel.setStyleGuide(.subHeader5)
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textColor = Color.mainForeground
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        setupLayout()
+    }
+
+    private func setupLayout() {
+        addSubview(imageView)
+        addSubview(headerLabel)
+        addSubview(descriptionLabel)
+
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+
+            headerLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            headerLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 28),
+            headerLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32),
+
+            descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            descriptionLabel.topAnchor.constraint(equalTo: headerLabel.lastBaselineAnchor, constant: 8),
+            descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32),
+            descriptionLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
---
+++ Holk/ListInsurance/InsurnaceSuggestion/View/ThinkOfSuggestionDetail/ThinkOfDetailCollectionHeaderView.swift
@@ -0,0 +1,84 @@
+//
+//  ThinkOfDetailCollectionHeaderView.swift
+//  Holk
+//
+//  Created by 张梦皓 on 2020-07-19.
+//  Copyright © 2020 Holk. All rights reserved.
+//
+
+import UIKit
+
+final class ThinkOfDetailCollectionHeaderView: UICollectionReusableView {
+    private let headerBackgroundView = UIView()
+    private let iconView = HolkIconView()
+    private let tagLabel = UILabel()
+    private let titleLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+
+    func configure(_ viewModel: InsuranceThinkOfDetailViewModel) {
+        iconView.imageView.image = viewModel.iconImage
+        iconView.backgroundColor = viewModel.iconImageBackgroundColor
+        tagLabel.text = viewModel.subInsuranceText
+        titleLabel.text = viewModel.title
+        headerBackgroundView.backgroundColor = viewModel.headerBackgroundViewColor
+    }
+
+    private func setup() {
+        clipsToBounds = true
+        backgroundColor = Color.secondaryBackground
+
+        layoutMargins = .init(top: 0, left: 26, bottom: 26, right: 26)
+
+        headerBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+
+        tagLabel.setStyleGuide(.titleHeader1)
+        tagLabel.numberOfLines = 0
+        tagLabel.textColor = Color.mainForeground
+        tagLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        titleLabel.setStyleGuide(.cardHeader3)
+        titleLabel.numberOfLines = 0
+        titleLabel.textColor = Color.secondaryLabel
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        setupLayout()
+    }
+
+    private func setupLayout() {
+        addSubview(headerBackgroundView)
+        headerBackgroundView.addSubview(iconView)
+        headerBackgroundView.addSubview(tagLabel)
+        headerBackgroundView.addSubview(titleLabel)
+
+        NSLayoutConstraint.activate([
+            headerBackgroundView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            headerBackgroundView.topAnchor.constraint(equalTo: topAnchor),
+            headerBackgroundView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            headerBackgroundView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            iconView.widthAnchor.constraint(equalToConstant: 52),
+            iconView.heightAnchor.constraint(equalToConstant: 52),
+            iconView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            iconView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+
+            tagLabel.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            tagLabel.topAnchor.constraint(equalTo: iconView.bottomAnchor, constant: 12),
+            tagLabel.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+
+            titleLabel.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: tagLabel.lastBaselineAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: headerBackgroundView.bottomAnchor, constant: -52)
+        ])
+    }
+}
