a29cfe28902cf427bca83047cad0604fc2a82d3e
Menghao Zhang <zmhand1@gmail.com>
2020-07-19 20:44:51 +0200

    view model updates

--- Holk/ListInsurance/InsurnaceSuggestion/ViewModel/InsuranceThinkOfDetailViewModel.swift
+++ Holk/ListInsurance/InsurnaceSuggestion/ViewModel/InsuranceThinkOfDetailViewModel.swift
@@ -11,21 +11,22 @@ import UIKit
 final class InsuranceThinkOfDetailViewModel {
     private var thinkOfSuggestion: ThinkOfSuggestion
 
-    var title: String
-    var subInsuranceText: String
-    var imageURL: URL?
-    var detailHeader: String
-    var detailDescription: String
-    var detailParagraphs: [Paragraph]
-    var mappedInsurance: Insurance?
-    var mappedSubInsurances: [Insurance.Segment]
+    let title: String
+    let subInsuranceText: String
+    // TODO: fix
+    private let detailParagraphs: [Paragraph]
+    private let mappedInsurance: Insurance?
+    private let mappedSubInsurances: [Insurance.Segment]
+
+    // TODO: fix
+    var iconImage: UIImage?
+    var iconImageBackgroundColor: UIColor?
+    var headerBackgroundViewColor: UIColor?
 
     init(thinkOfSuggestion: ThinkOfSuggestion, insurances: [Insurance]) {
         self.thinkOfSuggestion = thinkOfSuggestion
 
         title = thinkOfSuggestion.title
-        detailHeader = thinkOfSuggestion.details.header
-        detailDescription = thinkOfSuggestion.details.description
         detailParagraphs = thinkOfSuggestion.details.paragraphs
         subInsuranceText = thinkOfSuggestion.subInsurance
 
@@ -36,6 +37,27 @@ final class InsuranceThinkOfDetailViewModel {
         mappedSubInsurances = insurances.flatMap { insurance in
             insurance.segments.filter { segment in segment.kind == .travel }
         }
+
+        if let subInsurance = mappedSubInsurances.first {
+            switch subInsurance.kind {
+            case .home:
+                iconImage = UIImage.init(insuranceSegment: subInsurance)
+                iconImageBackgroundColor = Color.goodsInsuranceIconBackgroundColor
+                headerBackgroundViewColor = Color.goodsInsuranceBackgroundColor
+            case .travel:
+                iconImage = UIImage.init(insuranceSegment: subInsurance)
+                iconImageBackgroundColor = Color.travelInsuranceIconBackgroundColor
+                headerBackgroundViewColor = Color.travelInsuranceBackgroundColor
+            case .pets:
+                iconImage = nil
+                iconImageBackgroundColor = .systemPink
+                headerBackgroundViewColor = Color.mainHighlight
+            }
+        }
+    }
+
+    func makeThinkOfSuggestionBannerViewModel() -> ThinkOfSuggestionBannerViewModel {
+        return ThinkOfSuggestionBannerViewModel(thinkOfSuggestion: thinkOfSuggestion)
     }
 
     func makeAllThinkOfSuggestionParagraphViewModel() -> [ThinkOfSuggestionParagraphViewModel] {
@@ -55,7 +77,7 @@ final class InsuranceThinkOfDetailViewModel {
         return mappedSubInsurances.map(ThinkOfSubInsuranceViewModel.init(subInsurance: ))
     }
 
-    func makeThinkOfSubInsuranceViewModel(at indexPath: IndexPath) -> ThinkOfSubInsuranceViewModel {
+    private func makeThinkOfSubInsuranceViewModel(at indexPath: IndexPath) -> ThinkOfSubInsuranceViewModel {
         let subInsurance = mappedSubInsurances[indexPath.item]
         return ThinkOfSubInsuranceViewModel(subInsurance: subInsurance)
     }
