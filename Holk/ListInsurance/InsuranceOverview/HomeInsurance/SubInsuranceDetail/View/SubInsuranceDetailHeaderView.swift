b2d3e5f966b943aa7329a880d0487588ec9424be
Menghao Zhang <zmhand1@gmail.com>
2020-10-11 18:18:08 +0200

    Update the subinsurance detail header view

--- Holk/ListInsurance/InsuranceOverview/HomeInsurance/SubInsuranceDetail/View/SubInsuranceDetailHeaderView.swift
+++ Holk/ListInsurance/InsuranceOverview/HomeInsurance/SubInsuranceDetail/View/SubInsuranceDetailHeaderView.swift
@@ -1,9 +1,32 @@
 //
-//  HomeSubInsuranceHeaderView.swift
+//  SubInsuranceDetailHeaderView.swift
 //  Holk
 //
 //  Created by 张梦皓 on 2020-10-11.
 //  Copyright © 2020 Holk. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class SubInsuranceDetailHeaderView: UICollectionReusableView {
+    weak var subInsuranceDetailViewController: HomeSubInsuranceDetailViewController?
+
+    private let titleLabel = HolkLabel()
+    private let subtitle = HolkLabel()
+    private let descriptionLabel = HolkLabel()
+
+    private let coverIcon = HolkIconSelectionView()
+    private let gapsIcon = HolkIconSelectionView()
+    private let thinkOfsIcon = HolkIconSelectionView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
