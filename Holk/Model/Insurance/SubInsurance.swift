26e5acc46088a55b68adf98ce87d3cc2f571cc14
Menghao Zhang <zmhand1@gmail.com>
2020-12-06 15:59:56 +0100

    subinsurance parsing

--- Holk/Model/Insurance/SubInsurance.swift
+++ Holk/Model/Insurance/SubInsurance.swift
@@ -15,10 +15,33 @@ extension Insurance {
         }
 
         struct Item: Codable, Hashable, Equatable {
+            enum Segment: String, Codable, DefaultableDecodable, CaseIterable, Comparable {
+                static func < (lhs: Insurance.SubInsurance.Item.Segment, rhs: Insurance.SubInsurance.Item.Segment) -> Bool {
+                    return lhs.rawValue < rhs.rawValue
+                }
+
+                case home = "AT_HOME"
+                case outdoor = "OUT_OF_HOME"
+                case other
+
+                static var decodeFallbackValue = other
+            }
+
             let infoText: String
-            let maxAmount: String
+            let value: String
             let name: String
-            let result: String
+            let key: String
+            let iconUrl: URL
+            var segment: Segment = .other
+
+            private enum CodingKeys: String, CodingKey {
+                case infoText = "infoText"
+                case name = "name"
+                case value = "value"
+                case key = "key"
+                case iconUrl = "icon"
+                case segment = "segment"
+            }
         }
 
         enum Kind: String, Codable, DefaultableDecodable, CaseIterable, Comparable {
@@ -53,16 +76,20 @@ extension Insurance {
             }
         }
 
+        private var id: UUID
         let subtitle: String
         let title: String
         let iconUrl: URL
         let kind: Kind
+        // TODO: Remove the mock
+        let items: [Item]
 
         private enum CodingKeys: String, CodingKey {
             case kind = "type"
             case title = "title"
             case subtitle = "subTitle"
             case iconUrl = "icon"
+            case items = "itemDefinitionList"
         }
     }
 }
@@ -71,9 +98,25 @@ extension Insurance.SubInsurance {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
+        id = UUID()
         kind = try container.decode(Kind.self, forKey: .kind)
         subtitle = try container.decode(String.self, forKey: .subtitle)
         title = try container.decode(String.self, forKey: .title)
         iconUrl = try container.decode(URL.self, forKey: .iconUrl)
+        items = (try? container.decode([Item].self, forKey: .items)) ?? []
+    }
+}
+
+
+extension Insurance.SubInsurance.Item {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        infoText = try container.decode(String.self, forKey: .infoText)
+        value = try container.decode(String.self, forKey: .value)
+        name = try container.decode(String.self, forKey: .name)
+        key = try container.decode(String.self, forKey: .key)
+        iconUrl = try container.decode(URL.self, forKey: .iconUrl)
+        segment = (try? container.decode(Insurance.SubInsurance.Item.Segment.self, forKey: .segment)) ?? .other
     }
 }
